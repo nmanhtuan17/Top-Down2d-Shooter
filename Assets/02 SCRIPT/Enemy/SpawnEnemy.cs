7d758b52ac6a264e6df837d4a948283c9afaace7
unknown <manhtuan17403@gmail.com>
2022-12-22 19:10:11 +0700

    spawn enemy

---
+++ Assets/02 SCRIPT/Enemy/SpawnEnemy.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    float timeCountDown;
+    Vector2 border;
+    [SerializeField] GameObject enemyPrefab;
+    
+    [SerializeField] List<Vector2> posSpawns = new List<Vector2>();
+    Vector2 posSpawn;
+    [SerializeField] Vector2 randomPos;
+    void Start()
+    {
+        border = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    }
+
+    
+    void Update()
+    {
+        //Spawn Enemies
+        timeCountDown -= Time.deltaTime;
+        if (timeCountDown <= 0)
+        {
+            Spawn();
+            timeCountDown = Random.Range(2f, 2.5f);
+        }
+            
+        
+    }
+
+    void Spawn()
+    {
+        randomPos = new Vector2(Random.Range(0.1f, 0.5f), Random.Range(0.1f, 0.5f));
+        posSpawn = new Vector2 (Random.Range(-border.x, border.x), Random.Range(-border.y, border.y));
+        posSpawns.Add(posSpawn);
+
+        Instantiate(enemyPrefab, posSpawns[Random.Range(0, posSpawns.Count)], Quaternion.identity);
+    }
+    
+}
